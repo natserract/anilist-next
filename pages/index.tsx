9e14bc9271d2a597f7e8c59ad6ed77f92d3cc4ea
natserract <benjaminstwo@gmail.com>
2022-03-10 18:41:41 +0800

    fix: 🐛 React Component as default export in

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { HOMEQUERY } from './graphql'
+import { HOMEQUERY } from '../query/graphql'
 
 const Home: NextPage = ({ home }: any) => {
   return (
